bdbe92d19c81c39913198288b22c116169ca0e51
Mike McTighe <m.mctighe10@gmail.com>
2019-10-01 22:01:00 +0000

    displays fake markets

--- lib/market_watch/cli.rb
+++ lib/market_watch/cli.rb
@@ -2,8 +2,33 @@
 class MarketWatch::CLI
 
   def call
-    puts "See how the Market you wanna know about is doing!
-  select your Market:"
+    puts "Welcome, See how the Market you wanna know about is doing!"
+   list_markets
+
+
+
   end
+
+  def list_markets
+
+    puts <<-DOC.gsub /^\s*/, ''
+      1. s&p 500 (1%)
+      2. dow 30 (2%)
+      3. nasdaq (3%)
+      4. russell 2000 (4%)
+      5. crude oil (5%)
+      6. gold (6%)
+      7. silver (7%)
+      8. eur/usd (8%)
+      9. 10-yr bond (9%)
+      10. vix (10%)
+      11. gbp/usd (11%)
+      12. usd/jpy (12%)
+      13. bitcoin USD (13%)
+      14. FTSE 100 (14%)
+      15. nikkei 225 (15%)
+      DOC
+  end
+
 
 end
