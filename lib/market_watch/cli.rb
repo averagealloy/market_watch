55cb72e2eb4d955bcb1659776de75bf0f2b6cc33
Mike McTighe <m.mctighe10@gmail.com>
2019-10-02 15:14:21 +0000

    fleshed out market selector w fake data

--- lib/market_watch/cli.rb
+++ lib/market_watch/cli.rb
@@ -11,27 +11,61 @@ class MarketWatch::CLI
 
 
         puts <<-DOC.gsub /^\s*/, ''
-          1. s&p 500 - (1%)
-          2. dow 30 - (2%)
-          3. nasdaq - (3%)
-          4. russell 2000 - (4%)
-          5. crude oil - (5%)
-          6. gold - (6%)
-          7. silver - (7%)
-          8. eur/usd - (8%)
-          9. 10-yr bond - (9%)
-          10. vix - (10%)
-          11. gbp/usd - (11%)
-          12. usd/jpy - (12%)
-          13. bitcoin USD - (13%)
-          14. FTSE 100 - (14%)
-          15. nikkei 225 - (15%)
+          1 s&p 500 - (1%)
+          2 dow 30 - (2%)
+          3 nasdaq - (3%)
+          4 russell 2000 - (4%)
+          5 crude oil - (5%)
+          6 gold - (6%)
+          7 silver - (7%)
+          8 eur/usd - (8%)
+          9 10-yr bond - (9%)
+          10 vix - (10%)
+          11 gbp/usd - (11%)
+          12 usd/jpy - (12%)
+          13 bitcoin USD - (13%)
+          14 FTSE 100 - (14%)
+          15 nikkei 225 - (15%)
           DOC
         end
 
 
         def market_select
           puts "Select your Market:"
-        end
+          input = gets.strip
 
+        case input 
+      when "1"
+          puts "more info about your market1 ..."
+      when "2"
+          puts "more info about your market2 ..."
+      when "3"
+        puts "more info about your market3 ..."
+      when "4"
+        puts "more info about your market4 ..."
+      when "5"
+        puts "more info about your market5 ..."
+      when "6"
+        puts "more info about your market6 ..."
+      when "7"
+        puts "more info about your market7 ..."
+      when "8"
+        puts "more info about your market8 ..."
+      when "9"
+          puts "more info about your market9 ..."
+      when "10"
+          puts "more info about your market10 ..."
+      when "11"
+        puts "more info about your market11 ..."
+      when "12"
+        puts "more info about your market12 ..."
+      when "13"
+        puts "more info about your market13 ..."
+      when "14"
+        puts "more info about your market14 ..."
+      when "15"
+        puts "more info about your market15 ..."
+
+      end
+      end
 end
