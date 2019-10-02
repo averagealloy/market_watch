d06aaba760382a6c23488b788d22367efc6bbe8d
Mike McTighe <m.mctighe10@gmail.com>
2019-10-02 15:26:25 +0000

    added goodbye method that exits the program

--- lib/market_watch/cli.rb
+++ lib/market_watch/cli.rb
@@ -4,6 +4,7 @@ class MarketWatch::CLI
   def call
     list_markets
     market_select
+    goodbye
   end
 
   def list_markets
@@ -31,41 +32,46 @@ class MarketWatch::CLI
 
 
         def market_select
-          puts "Select your Market:"
-          input = gets.strip
+          puts "Select your Market or type exit to enter :"
+          input = nil
+          while input != "exit"
+            input = gets.strip.downcase
+              case input
+              when "1"
+                puts "more info about your market1 ..."
+              when "2"
+                puts "more info about your market2 ..."
+              when "3"
+                puts "more info about your market3 ..."
+              when "4"
+                puts "more info about your market4 ..."
+              when "5"
+                puts "more info about your market5 ..."
+              when "6"
+                puts "more info about your market6 ..."
+              when "7"
+                puts "more info about your market7 ..."
+              when "8"
+                puts "more info about your market8 ..."
+              when "9"
+                puts "more info about your market9 ..."
+              when "10"
+                puts "more info about your market10 ..."
+              when "11"
+                puts "more info about your market11 ..."
+              when "12"
+                puts "more info about your market12 ..."
+              when "13"
+                puts "more info about your market13 ..."
+              when "14"
+                puts "more info about your market14 ..."
+              when "15"
+                puts "more info about your market15 ..."
 
-        case input 
-      when "1"
-          puts "more info about your market1 ..."
-      when "2"
-          puts "more info about your market2 ..."
-      when "3"
-        puts "more info about your market3 ..."
-      when "4"
-        puts "more info about your market4 ..."
-      when "5"
-        puts "more info about your market5 ..."
-      when "6"
-        puts "more info about your market6 ..."
-      when "7"
-        puts "more info about your market7 ..."
-      when "8"
-        puts "more info about your market8 ..."
-      when "9"
-          puts "more info about your market9 ..."
-      when "10"
-          puts "more info about your market10 ..."
-      when "11"
-        puts "more info about your market11 ..."
-      when "12"
-        puts "more info about your market12 ..."
-      when "13"
-        puts "more info about your market13 ..."
-      when "14"
-        puts "more info about your market14 ..."
-      when "15"
-        puts "more info about your market15 ..."
-
+              end
+            end
       end
+      def goodbye
+        puts "The market changes so come back tomorrow to see how you're doing!"
       end
 end
