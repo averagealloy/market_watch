40ca8669d0c95b5d18ece3e1b73177f6e2a07986
Mike McTighe <m.mctighe10@gmail.com>
2019-10-03 16:07:22 +0000

    setting up where im doing scraping

--- lib/market_watch/market.rb
+++ lib/market_watch/market.rb
@@ -5,22 +5,17 @@ attr_accessor :name, :BMI, :url
 
 def self.today
   #should return a bunuch of markets
+  #scrape yahoo finance to get the desired information
 
-          market_1 = self.new
-          market_1.name = "s&p 500"
-          market_1.BMI = "1%"
+  self.scrape_markets
+      end
 
-          market_1.url = "https://finance.yahoo.com"
+  def self.scrape_markets
+    markets = []
 
-          market_2 = self.new
-          market_2.name = "dow 30"
-          market_2.BMI = "2%"
 
-          market_2.url = "https://finance.yahoo.com"
 
-          [market_1, market_2 ]
-            #test = "test"
 
-        end
-
+    markets 
+  end
 end
